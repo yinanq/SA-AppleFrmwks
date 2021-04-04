75c28dd8bd71d59019ba969a9f60e10597ab1f17
Yinan Qiu <yinanq@users.noreply.github.com>
2021-04-03 20:10:37 -0700

    project created

---
+++ AppleFrmwks/AppleFrmwks/AppleFrmwksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrmwksApp.swift
+//  AppleFrmwks
+//
+//  Created by Yinan Qiu on 4/3/21.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrmwksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
