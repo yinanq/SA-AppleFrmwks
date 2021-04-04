04e133312acc4e4b381661418d25caad33d57574
Yinan Qiu <yinanq@users.noreply.github.com>
2021-04-03 20:27:54 -0700

    created Framework object

--- AppleFrmwks/AppleFrmwks/AppleFrmwksApp.swift
+++ AppleFrmwks/AppleFrmwks/AppleFrmwksApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AppleFrmwksApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FrameworkGridView()
         }
     }
 }
---
+++ AppleFrmwks/AppleFrmwks/FrameworkGridView.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworkGridView.swift
+//  AppleFrmwks
+//
+//  Created by Yinan Qiu on 4/3/21.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+    }
+}
